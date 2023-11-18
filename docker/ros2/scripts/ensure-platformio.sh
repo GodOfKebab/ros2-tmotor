ae66221d781354b9a0a26b9c5cb64eba4702f9d6
Yaşar İdikut <yasar8000@gmail.com>
2023-11-18 17:07:15 -0500

    fix platformio installation

--- docker/ros2/scripts/ensure-platformio.sh
+++ docker/ros2/scripts/ensure-platformio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! [ -f /root/.platformio/penv/bin/pio ]; then
   # Install platformio the recommended way, then move for caching
-  apt-get install -y python3-venv python3-distutils
+  apt update && apt install -y python3-venv python3-distutils
   curl -fsSL -o get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py
   python3 get-platformio.py
   rm get-platformio.py
